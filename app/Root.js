2f0011e017b691d74d355b575ab348f663a6ea48
dinidi-maneesha <dinidimaneesha4@gmail.com>
2022-08-01 20:18:11 +0530

    initial commit

---
+++ app/Root.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import RootNavigation from './navigation/navigationRes';
+
+export default class Root extends Component {
+  render() {
+    return <RootNavigation />;
+  }
+}
